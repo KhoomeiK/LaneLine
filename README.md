d633f6e19c4264ca0b3c2cb513d022929a56c6ae
KhoomeiK <rohan.pandey@gmail.com>
2019-10-26 16:11:34 -0700

    cleaned

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
 # LaneLine
+
+## Demo
+
+- Connect to Ford external display, mirrors
+- Open map page (GCP Maps SDK) and set route to destination (rideOS)
+- Confirm route and jump to dashcam demo
+- Starts 30 second timer and counts down distance as car "moves" toward exit
+- 25: Ford ahead in our lane slows down and space open to our right
+- 20: Switch lane to right and speed past cars on left
+- 15: Ford ahead in our lane detects open spot to left
+- 10: Continue to speed ahead
+- 5: Switch into right lane
+- 0: Take exit
+- Car crash situation
--- ViroSample/js/HelloWorldSceneAR.js
+++ ViroSample/js/HelloWorldSceneAR.js
@@ -7,7 +7,8 @@ import { StyleSheet } from 'react-native';
 import {
   ViroARScene,
   ViroText,
-  ViroConstants
+  ViroConstants,
+  ViroImage
 } from 'react-viro';
 
 export default class HelloWorldSceneAR extends Component {
@@ -27,7 +28,24 @@ export default class HelloWorldSceneAR extends Component {
   render() {
     return (
       <ViroARScene onTrackingUpdated={this._onInitialized} >
-        <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} />
+        {/* <ViroText text={this.state.text} scale={[.5, .5, .5]} position={[0, 0, -1]} style={styles.helloWorldTextStyle} /> */}
+        {/* <ViroGeometry position={[-2.5, 0, -0.1]} scale={[1.0, 1.0, 1.0]}
+          vertices={[[1, 0, -1], [-1, 0, -1], [1, 5, -1],
+          [-1, 5, -1], [2, 5, -1], [-2, 5, -1], [0, 7, -1]]}
+          normals={[[0, 0, 1], [0, 0, 1], [0, 0, 1]]}
+          texcoords={[[1, 0], [0, 1], [-1, 0]]}
+          triangleIndices={[[0, 1, 2]]} /> */}
+        {/* <ViroPolygon rotation={[-90, 0, 0]}
+          position={[0, 0, -1]}
+          vertices={[[-1, 0], [0, 1], [1, 0]]}
+        /> */}
+        <ViroImage
+          height={1}
+          width={1}
+          position={[0, 0, -1]}
+          rotation={[-45, 0, 0]}
+          source={require('./res/arrow.png')}
+        />
       </ViroARScene>
     );
   }
@@ -35,7 +53,7 @@ export default class HelloWorldSceneAR extends Component {
   _onInitialized(state, reason) {
     if (state == ViroConstants.TRACKING_NORMAL) {
       this.setState({
-        text: "LaneLine fuk ya!"
+        text: ""
       });
     } else if (state == ViroConstants.TRACKING_NONE) {
       // Handle loss of tracking
